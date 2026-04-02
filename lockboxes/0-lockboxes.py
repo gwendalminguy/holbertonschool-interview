555b72e92b47cc99745352de55770818db43aefc
Gwendal <gwendalminguy@outlook.fr>
2026-04-02 14:31:16 +0200

    Adjust logic to decrement number of newly found keys once a new box has been explored instead of resetting it

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -17,12 +17,13 @@ def canUnlockAll(boxes: list) -> bool:
 
     for _ in boxes:                                             # For as many times as there are boxes.
         for key in keys[found - newly_found:]:                  # For each new key found.
-            newly_found = 0                                     # Reset number of newly found keys.
             for new in boxes[key]:                              # For each new key found in the box newly opened box.
-                if new not in keys and new < len(boxes):       # Add each key only if not already in keys and if it opens an actual box.
-                    keys.extend(boxes[key])
-                    newly_found += 1
+                if new not in keys and new < len(boxes):        # Add each key only if not already in keys and if it opens an actual box.
+                    keys.append(new)
+                    newly_found += 1                            # Incrementing count of newly found keys when a new key is found.
                     found += 1
+            newly_found -= 1                                    # Decrementing count of newly found keys once the box has been explored.
+            #print(keys)
 
     for key in keys:
         openable[key] = True
