4e61d1903d9c2b121514cda4071d4ac64f2f8187
Gwendal <gwendalminguy@outlook.fr>
2026-04-17 21:43:30 +0200

    Handle malloc return before values assignation

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -15,13 +15,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new = malloc(sizeof(binary_tree_t));
 
+	if (new == NULL)
+		return (NULL);
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-
-	if (new == NULL)
-		return (NULL);
 
 	return (new);
 }
