88937a9f95a3bfc60e4b891861afdec86344ae79
Gwendal <gwendalminguy@outlook.fr>
2026-05-19 09:22:38 +0200

    Replace space indentation with tab indentation

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -9,24 +9,24 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    /* Summing both grids into grid1 */
-    for (i = 0 ; i < 3 ; i++)
-    {
-        for (j = 0 ; j < 3 ; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	/* Summing both grids into grid1 */
+	for (i = 0 ; i < 3 ; i++)
+	{
+		for (j = 0 ; j < 3 ; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 
-    /* Computing new sandpiles by iteration until stable */
-    while (!is_stable(grid1))
-    {
-        print_sandpiles(grid1);
-        update_sandpiles(grid1);
-    }
+	/* Computing new sandpiles by iteration until stable */
+	while (!is_stable(grid1))
+	{
+		print_sandpiles(grid1);
+		update_sandpiles(grid1);
+	}
 }
 
 /**
@@ -37,19 +37,19 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  */
 bool is_stable(int grid[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    for (i = 0 ; i < 3 ; i++)
-    {
-        for (j = 0 ; j < 3 ; j++)
-        {
-            if (grid[i][j] > 3)
-                return false;
-        }
-    }
+	for (i = 0 ; i < 3 ; i++)
+	{
+		for (j = 0 ; j < 3 ; j++)
+		{
+			if (grid[i][j] > 3)
+				return (false);
+		}
+	}
 
-    return (true);
+	return (true);
 }
 
 /**
@@ -60,45 +60,45 @@ bool is_stable(int grid[3][3])
  */
 void update_sandpiles(int grid1[3][3])
 {
-    int i = 0;
-    int j = 0;
-    int grid2[3][3] = {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    };
+	int i = 0;
+	int j = 0;
+	int grid2[3][3] = {
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0}
+	};
 
-    for (i = 0 ; i < 3 ; i++)
-    {
-        for (j = 0 ; j < 3 ; j++)
-        {
-            while (grid1[i][j] > 3)
-            {
-                grid1[i][j] -= 4;
-                grid2[i][j] += 4;
-            }
-        }
-    }
+	for (i = 0 ; i < 3 ; i++)
+	{
+		for (j = 0 ; j < 3 ; j++)
+		{
+			while (grid1[i][j] > 3)
+			{
+				grid1[i][j] -= 4;
+				grid2[i][j] += 4;
+			}
+		}
+	}
 
-    for (i = 0 ; i < 3 ; i++)
-    {
-        for (j = 0 ; j < 3 ; j++)
-        {
-            while (grid2[i][j] > 3)
-            {
-                grid2[i][j] -= 4;
+	for (i = 0 ; i < 3 ; i++)
+	{
+		for (j = 0 ; j < 3 ; j++)
+		{
+			while (grid2[i][j] > 3)
+			{
+				grid2[i][j] -= 4;
 
-                if (i >= 1)
-                    grid1[i - 1][j] += 1;
-                if (j >= 1)
-                    grid1[i][j - 1] += 1;
-                if (i <= 1)
-                    grid1[i + 1][j] += 1;
-                if (j <= 1)
-                    grid1[i][j + 1] += 1;
-            }
-        }
-    }
+				if (i >= 1)
+					grid1[i - 1][j] += 1;
+				if (j >= 1)
+					grid1[i][j - 1] += 1;
+				if (i <= 1)
+					grid1[i + 1][j] += 1;
+				if (j <= 1)
+					grid1[i][j + 1] += 1;
+			}
+		}
+	}
 }
 
 /**
@@ -108,18 +108,18 @@ void update_sandpiles(int grid1[3][3])
  */
 void print_sandpiles(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    printf("=\n");
+	printf("=\n");
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
