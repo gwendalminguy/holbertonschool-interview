d9cb65df6bdbdd9331611b8c3cdbf7f32bbbc9f9
Gwendal <gwendalminguy@outlook.fr>
2026-06-19 15:19:10 +0200

    Add get_length and raise prototypes

--- palindrome_integer/palindrome.h
+++ palindrome_integer/palindrome.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int get_length(unsigned long n);
+unsigned long raise(unsigned long value, int power);
 int is_palindrome(unsigned long n);
 
 #endif /* PALINDROME_H */
