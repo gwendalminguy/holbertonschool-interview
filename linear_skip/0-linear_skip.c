5d75a8e7c27e048d57d1f7fc497e505a86d99208
Gwendal <gwendalminguy@outlook.fr>
2026-08-29 15:31:10 +0200

    Create function searching for a value in a skip list of integers

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,53 @@
+#include "search.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ *
+ * @head: pointer to the head of the list
+ * @value: value to search for
+ *
+ * Return: ...
+ */
+skiplist_t *linear_skip(skiplist_t *head, int value)
+{
+	size_t upper;
+	skiplist_t *current, *last;
+
+	if (!head)
+		return (NULL);
+
+	last = head;
+	current = head;
+
+	/* First search through express lane */
+	while (current && current->n < value)
+	{
+		last = current;
+		current = current->express;
+
+		if (current)
+			printf("Value checked at index [%li] = [%i]\n", current->index, current->n);
+
+		if (current && current->n > value)
+		{
+			upper = current->index;
+
+			printf("Value found between indexes ");
+			printf("[%li] and [%li]\n", last->index, current->index);
+		}
+	}
+	current = last;
+
+	/* Second search through normal lane */
+	while (current && current->index <= upper)
+	{
+		printf("Value checked at index [%li] = [%i]\n", current->index, current->n);
+
+		if (current->n == value)
+			return (current);
+
+		current = current->next;
+	}
+
+	return (NULL);
+}
