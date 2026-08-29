eedeecd6eacda55e0773234aa704d08bf2e2f12b
Gwendal <gwendalminguy@outlook.fr>
2026-08-29 15:51:44 +0200

    Correct first search steps order

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -16,29 +16,28 @@ skiplist_t *linear_skip(skiplist_t *head, int value)
 	if (!head)
 		return (NULL);
 
+	/* First search through express lane */
 	while (current)
 	{
+		printf("Value checked at index [%li] = [%i]\n", current->index, current->n);
+
+		if (current->n >= value)
+		{
+			upper = current->index;
+
+			printf("Value found between indexes ");
+			printf("[%li] and [%li]\n", last->index, current->index);
+
+			break;
+		}
+
 		last = current;
 		current = current->express;
-
-		if (current)
-		{
-			printf("Value checked at index [%li] = [%i]\n", current->index, current->n);
-
-			if (current->n >= value)
-			{
-				upper = current->index;
-
-				printf("Value found between indexes ");
-				printf("[%li] and [%li]\n", last->index, current->index);
-
-				break;
-			}
-		}
 	}
 
 	current = last;
 
+	/* Second search through normal lane */
 	while (current && current->index <= upper)
 	{
 		printf("Value checked at index [%li] = [%i]\n", current->index, current->n);
